a9c84dd08744b351a49fa1d095cb3393fcc94fd6
David Wood <david.p.wood@gmail.com>
2015-08-27 08:12:30 +0100

    Add task hooks for build and test

---
+++ src/_ref.d.ts
@@ -0,0 +1,2 @@
+/// <reference path="../typings/tsd.d.ts" />
+/// <reference path="../custom_typings/tsd.d.ts" />
---
+++ src/test/app.test.ts
@@ -0,0 +1,15 @@
+/// <reference path="../_ref.d.ts" />
+
+import assert = require('assert')
+
+describe('Some suite', () => {
+	
+	describe('Some functionality', () => {
+		
+		it('should really just pass', () => {
+			assert.ok(true)
+		})
+		
+	})
+	
+})
